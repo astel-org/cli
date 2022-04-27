b72a3533cc2fef1b9e56a9b5a90edfdb334cc895
sumitkolhe <thesumitkolhe@gmail.com>
2022-04-27 21:29:55 +0530

    fix: utils not ignored if dir present

--- src/templates/install.template.ts
+++ src/templates/install.template.ts
@@ -1,28 +1,36 @@
 import type { ComponentNames } from '../helpers/generate-name'
 
 export const getInstallTemplate = (components: ComponentNames[]) => {
-  return `
-      ${components
-        .map((component) => `import {${component.formattedName}} from './${component.name}';`)
-        .join('\n')}
-      import type {App} from 'vue';
-    
-      const components = [${components.map((component) => component.formattedName)}];\n
-  
-      const install = (app: App) => {
-        components.forEach((component) => {
-          component.name &&
-            app.component(component.name, component)
-        })
-      }\n
-  
-      export default { install };
+  const componentList: string[] = []
+  let template = `import type {App} from 'vue';\n`
+  let utilsExport = ``
+  let exportedComponents = ``
 
-      ${components
-        .map(
-          (component) =>
-            `export {default as ${component.formattedName}} from './${component.name}';`
-        )
-        .join('\n')}
-      `
+  components.forEach((component) => {
+    if (component.name === 'utils') {
+      utilsExport += `export * from './utils/composables';\n`
+      return
+    }
+
+    componentList.push(component.formattedName)
+
+    template += `import {${component.formattedName}} from './${component.name}';\n`
+    exportedComponents += `export {default as ${component.formattedName}} from './${component.name}';\n`
+  })
+
+  template += `\nconst components = [${componentList}];\n\n 
+  
+  const install = (app: App) => {
+    components.forEach((component) => {
+      component.name &&
+        app.component(component.name, component)
+    })
+  }\n
+  `
+  template += `
+  ${exportedComponents};\n
+  ${utilsExport}
+  export default { install };\n`
+
+  return template
 }
